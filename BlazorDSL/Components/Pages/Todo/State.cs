8eb908678bc6934c9cccdf698b3ec8bd795320d6
Alexander Böhm <alexander.boehm@alebo.de>
2025-11-30 20:57:03 +0100

    Implementierung des Models des MVU-Architekturmusters für die Beispiel-Applikation

---
+++ BlazorDSL/Components/Pages/Todo/State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+
+namespace BlazorDSL.Components.Pages.Todo;
+
+public record State(
+    ImmutableList<TodoItem> TodoItems,
+    string InputText,
+    string FilterText
+);
+
+public record TodoItem(
+    int Id,
+    string Text,
+    bool Done
+);
