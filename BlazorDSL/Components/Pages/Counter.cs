ecf11b7e22c67ac5eebe59d8bf1f42f20c55b347
Alexander Böhm <alexander.boehm@alebo.de>
2025-11-30 20:57:03 +0100

    Rudimentäre DSL mit Extension Methods

--- BlazorDSL/Components/Pages/Counter.cs
+++ BlazorDSL/Components/Pages/Counter.cs
@@ -7,22 +7,27 @@ using Microsoft.AspNetCore.Components.Web;
 [Route("/counter")]
 public partial class Counter : ComponentBase
 {        
-    protected override void BuildRenderTree(RenderTreeBuilder __builder)
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        __builder.AddMarkupContent(0, "<h1>Counter</h1>\r\n\r\n");
-        __builder.OpenElement(1, "div");
-        __builder.AddAttribute(2, "class", "box");
-        __builder.OpenElement(3, "p");
-        __builder.AddContent(4, "Current count: ");
-        __builder.AddContent(5, currentCount);
-        __builder.CloseElement();
-        __builder.AddMarkupContent(6, "\r\n    ");
-        __builder.OpenElement(7, "button");
-        __builder.AddAttribute(8, "class", "btn btn-primary");
-        __builder.AddAttribute(9, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, IncrementCount));
-        __builder.AddContent(10, "Click me");
-        __builder.CloseElement();
-        __builder.CloseElement();
+        builder
+            .h1(1, "Counter")
+            .div(2, [
+                new KeyValuePair<string, object>("class", "box")],
+                inner =>
+                    inner
+                    .p(3, "Current count: " + currentCount)
+                    .button(4, [
+                        new KeyValuePair<string, object>("class", "btn btn-primary"),
+                        new KeyValuePair<string, object>(
+                            "onclick",
+                            EventCallback.Factory.Create<MouseEventArgs>(
+                                this,
+                                IncrementCount
+                            )
+                        )],
+                        "Click me"
+                    )
+                );
     }
 
     private int currentCount = 0;
---
+++ BlazorDSL/RenderTreeBuilderExtensionMethods.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorDSL
+{
+    public static class RenderTreeBuilderExtensionMethods {
+        public static RenderTreeBuilder h1(this RenderTreeBuilder b, int line, string text) {
+            // eine Zeile benötigt mehrere Sequenz-Nummern
+            b.OpenRegion(line);
+            b.OpenElement(0, "h1");
+            b.AddContent(1, text);
+            b.CloseElement();
+            b.CloseRegion();
+            return b;
+        }
+
+        public static RenderTreeBuilder div(this RenderTreeBuilder b, int line, IEnumerable<KeyValuePair<string, object>> attributes, Action<RenderTreeBuilder> inner) {
+            // eine Zeile benötigt mehrere Sequenz-Nummern
+            b.OpenRegion(line);
+            b.OpenElement(0, "div");
+            b.AddMultipleAttributes(1, attributes);
+            b.OpenRegion(2);
+            inner(b);
+            b.CloseRegion();
+            b.CloseElement();
+            b.CloseRegion();
+            return b;
+        }
+
+        public static RenderTreeBuilder p(this RenderTreeBuilder b, int line, string text) {
+            // eine Zeile benötigt mehrere Sequenz-Nummern
+            b.OpenRegion(line);
+            b.OpenElement(0, "p");
+            b.AddContent(1, text);
+            b.CloseElement();
+            b.CloseRegion();
+            return b;
+        }
+
+        public static RenderTreeBuilder button(this RenderTreeBuilder b, int line, IEnumerable<KeyValuePair<string, object>> attributes, string text) {
+            // eine Zeile benötigt mehrere Sequenz-Nummern
+            b.OpenRegion(line);
+            b.OpenElement(0, "button");
+            b.AddMultipleAttributes(1, attributes);
+            b.AddContent(2, text);
+            b.CloseElement();
+            b.CloseRegion();
+            return b;
+        }
+    }
+}
