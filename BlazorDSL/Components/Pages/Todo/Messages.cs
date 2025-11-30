2ad7b9e034ea0b16b516c41adff68f4df09d4037
Alexander Böhm <alexander.boehm@alebo.de>
2025-11-30 20:57:03 +0100

    Implementierung der Messages des MVU-Architekturmusters für die Beispiel-Applikation

---
+++ BlazorDSL/Components/Pages/Todo/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+
+namespace BlazorDSL.Components.Pages.Todo;
+
+public abstract record Message();
+record AddItem(string text) : Message;
+record ItemAdded(TodoItem item) : Message;
+record RemoveItem(TodoItem item) : Message;
+record SetItemStatus(TodoItem item, bool status) : Message;
+record ChangeItemText(TodoItem item, string text) : Message;
+record RemoveDoneItems() : Message;
+record DoneItemsRemoved(ImmutableList<int> ids) : Message;
+record SetInputText(string text) : Message;
+record ItemsFromDatabaseLoaded(ImmutableList<TodoItem> items) : Message;
+record SetFilter(string filter) : Message;
