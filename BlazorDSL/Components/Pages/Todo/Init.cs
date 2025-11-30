57229611e27bf143014f51ef4b87ba6c9ebd0c21
Alexander Böhm <alexander.boehm@alebo.de>
2025-11-30 20:57:03 +0100

    Implementierung der Funktion Init des MVU-Architekturmusters für die Beispiel-Applikation

---
+++ BlazorDSL/Components/Pages/Todo/Init.cs
@@ -0,0 +1,17 @@
+﻿using Lib.MVU;
+using System.Collections.Immutable;
+
+namespace BlazorDSL.Components.Pages.Todo;
+
+partial class TodoPage
+{
+    public static (State, Command<Message>) Init() =>
+        (InitState(), Commands.LoadItemsFromDatabase());
+
+    static State InitState() =>
+        new State(
+            TodoItems: ImmutableList<TodoItem>.Empty,
+            InputText: "",
+            FilterText: ""
+        );
+}
